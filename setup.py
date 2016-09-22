565dfcf4bcc044ec21c84af67cb3309e95c104a1
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2016-09-22 23:29:44 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import sys
+
+from setuptools import find_packages, setup
+
+
+needs_pylint = 'lint' in sys.argv
+needs_pytest = bool({'pytest', 'test', 'ptr'}.intersection(sys.argv))
+setuptools_lint = ['setuptools-lint'] if needs_pylint else []
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+setup(
+    name='gamebook',
+    version='1.0.dev0',
+    author='Eugen Zagorodniy',
+    author_email='e.zagorodniy@gmail.com',
+    description='Extract snap counts from NFL players',
+
+    packages=find_packages(),
+    python_requires='>=2.7,<3',
+    install_requires=(
+        'pdfminer',
+    ),
+    setup_requires=[] + pytest_runner + setuptools_lint,
+    tests_require=(
+        'pytest',
+    ),
+)
