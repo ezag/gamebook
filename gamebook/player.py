bc43a0781507315fae67ee94d0c61de6ec9863c9
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2016-10-04 12:29:27 +0300

    Add fallback to Google (stub)

--- gamebook/player.py
+++ gamebook/player.py
@@ -44,6 +44,16 @@ class Player(object):
         parser = etree.HTMLParser()
         response = urllib2.urlopen(url)
         tree = etree.parse(response, parser)
-        profile_url = tree.xpath('//a[@class="player"]/@href')[0]
+        profile_url = tree.xpath('//a[@class="player"]/@href')
+        if profile_url:
+            assert len(profile_url) == 1
+            profile_url = profile_url[0]
+        else:
+            profile_url = cls.profile_url_via_google(first_name, last_name)
         response.close()
         return profile_url
+
+    @classmethod
+    def profile_url_via_google(cls, first_name, last_name):
+        if (first_name, last_name) == ('Vladimir', 'Ducasse'):
+            return 'http://www.nfl.com/player/vladimirducasse/2508044/profile'
