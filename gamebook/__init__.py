15b6f11de7f18977fecdeb467aadcd74b92b2a07
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2016-10-05 19:18:40 +0300

    Factor out get_rows

--- gamebook/__init__.py
+++ gamebook/__init__.py
@@ -40,44 +40,54 @@ def pdf_to_csv():
             out.writerow(csvrow)
 
 
-def url_to_csv():
-    url = sys.argv[1]
+field_names = (
+    'gamekey',
+    'player_id',
+    'player_name',
+    'position',
+    'team',
+    'off_snaps',
+    'off_pct',
+    'def_snaps',
+    'def_pct',
+    'spt_snaps',
+    'spt_pct',
+)
+
+
+def get_rows(url):
+    gamekey = url.rsplit('/', 2)[-2]
     response = urllib2.urlopen(url)
     pdf = StringIO(response.read())
     response.close()
     gb = GamebookParser(pdf)
     parsed = gb.extract_playtime_percentage()
-    out = csv.writer(sys.stdout)
-    out.writerow((
-        'team_name',
-        'player_id',
-        'player_name',
-        'position',
-        'off_snaps',
-        'off_pct',
-        'def_snaps',
-        'def_pct',
-        'spt_snaps',
-        'spt_pct',
-    ))
     for team in parsed:
         team_name, data = team
         player_names = [row.player_name for row in data]
         players_gsis_ids = Player.gsis_ids(url, player_names)
         for row, gsis_id in zip(data, players_gsis_ids):
-            csvrow = (
-                team_name,
+            yield dict(zip(field_names, (
+                gamekey,
                 gsis_id,
                 row.player_name,
                 row.position,
+                team_name,
                 row.off_snaps,
                 row.off_pct,
                 row.def_snaps,
                 row.def_pct,
                 row.spt_snaps,
                 row.spt_pct,
-            )
-            out.writerow(csvrow)
+            )))
+
+
+def url_to_csv():
+    url = sys.argv[1]
+    out = csv.writer(sys.stdout)
+    out.writerow(field_names)
+    for row in get_rows(url):
+        out.writerow([row[key] for key in field_names])
 
 
 def create_table():
